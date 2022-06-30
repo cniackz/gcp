00a618296e847ca307fdfbfe64f5e1bfe3eb11e7
Randolph Kahle <randy@doit-intl.com>
2022-06-30 10:14:32 -0400

    Initial GCP restructuring

--- benchmarking_scripts/gcp/benchmark_gcp_test.sh
+++ benchmarking_scripts/gcp/benchmark_gcp_test.sh
@@ -1,4 +1,11 @@
 #!/bin/zsh
+#
+# File: benchmark_gcp_test.sh
+#
+# Purpose: Drive min.io performance testing and configuration management on GCP
+#
+#
+
 
 source common.sh
 
@@ -28,8 +35,16 @@ SPEEDTEST_RESULT=
 # COMMAND_RESULT=
 # SPEEDTEST_RESULT=
 
+
+
+# (rsk: 2022-06-30) 
+#  A full cleanup of potential left over resources in GCP is left as an enhancement. 
+#  For now we will presume the script runs to completion
+#
+
+
 # Step 0
-kill_previous_gcp_instance 0
+
 # Remove firewall rules to eliminate network dependencies so we can remove VPCs
 remove_project_filewall_rules
 
@@ -38,29 +53,45 @@ remove_default_network
 
 # Remove the custom VPC created for testing
 remove_test_network
+
+
+
 # Step 1
-create_gcp_instance 1
 
-# Step 2
-wait_until_gcp_instances_are_ready_state 2
+# Create a custom VPC and subnetwork for the tests
+create_test_network
 
-# Step 3
-gcp_add_hosts_to_etc 3
+# Create minimal set of firewall rules for the test
+# (rsk: 2022-06-30)
+#  Create SSH connection "allow" firewall rule
+#  Others will be required when I understand the TCP / UDP connections between min.io servers and clients
+create_firewall_rules
 
-# Step 4
-mount_and_format_gcp_disks 4
+
+# Create the requested instances and the associated disks
+# Disks are created, mounted and formatted to ext4 in this process
+create_instances
+
+
+# Step 0
+#kill_previous_gcp_instance 0
+
+# Step 1
+#create_gcp_instance 1
+
 
 # Step 5
-run_minio_distributed_in_gcp_instances 5
+#run_minio_distributed_in_gcp_instances 5
 
 # Step 6
-make_sure_minio_is_running_on_each_gcp_node 6
+#make_sure_minio_is_running_on_each_gcp_node 6
 
 # Step 7
-set_mc_alias_in_gcp_instance 7
+#set_mc_alias_in_gcp_instance 7
 
 # Step 8
-run_speed_test_on_gcp_instance 8
+#run_speed_test_on_gcp_instance 8
 
 # Step 9
-kill_previous_gcp_instance 9
+#kill_previous_gcp_instance 9
+
