57903bde0106bf6f16a9122462e6e89dde0345da
Randolph Kahle <randy@doit-intl.com>
2022-07-20 15:39:30 -0400

    Add script test-configuration.sh
    This script contains the configuration for tests. It is used by the new family a creation and deletion scripts.

---
+++ benchmarking_scripts/gcp/test-configuration.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# File: test-configuration.sh
+#
+# Purpose: Hold configuration information for tests and clean-up scripts
+#
+#
+
+TEST_NETWORK=test-network
+TEST_SUBNET=test-subnetwork
+REGION=us-central1
+ZONE=us-central1-a
+
+NAME_PREFIX=min
+NAME_SUFFIX_START_NUMBER=1
+DISK_NAME_PREFIX=disk 
+DISK_SUFFIX_START_NUMBER=1
+
+MINIO_ADMIN_USER=minioadmin
+MINIO_ADMIN_PASSWORD=minioadmin
+
